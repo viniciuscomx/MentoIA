b732c7fc770844ea2fdf995ebfc573dd848689fc
viniciuscomx <gomesxavier619@gmail.com>
2025-05-14 16:20:46 -0300

    atualização no server

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
 require('dotenv').config();
 const express = require('express');
-const authRoutes = require('./routes/authRoutes');
+const RouterAuth = require('./routes/authRoutes');
 const trailRoutes = require('./routes/trailRoutes');
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/auth', authRoutes);
+app.use('/api/auth', RouterAuth);
 app.use('/api/trails', trailRoutes);
 
 const PORT = process.env.PORT || 3000;
